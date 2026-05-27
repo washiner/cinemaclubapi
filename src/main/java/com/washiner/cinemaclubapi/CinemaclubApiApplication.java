61f1a665a436923ac22c99a1032ac8e80640b7a1
Washiner <washiner@gmail.com>
2026-05-27 16:52:59 +0900

    chore: configura pomxml com todas configuraçoes do projeto

---
+++ src/main/java/com/washiner/cinemaclubapi/CinemaclubApiApplication.java
@@ -0,0 +1,13 @@
+package com.washiner.cinemaclubapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaclubApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemaclubApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/washiner/cinemaclubapi/CinemaclubApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.washiner.cinemaclubapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaclubApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
